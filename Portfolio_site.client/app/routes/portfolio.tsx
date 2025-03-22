6c3619561ce7e03094501388b2ce113d85ed6051
Evan Holloway <eholloway00@gmail.com>
2025-03-21 17:07:23 -0700

    adds material ui, adds portfolio page component

---
+++ Portfolio_site.client/app/routes/portfolio.tsx
@@ -0,0 +1,59 @@
+import type { Route } from "./+types/portfolio";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "The Portfolio" },
+    { name: "description", content: "The Portfolio!" },
+  ];
+}
+
+export default function Portfolio() {
+    const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+    return (<>
+        <p>Hello there</p>
+        <div>
+            <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+        </div>
+    </>)
+        
+  }
