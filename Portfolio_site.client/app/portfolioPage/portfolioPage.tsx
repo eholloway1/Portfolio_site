0a739a04ff12944ef8771b749ea8ec8f65c6c32f
Evan Holloway <eholloway00@gmail.com>
2025-03-25 08:11:23 -0700

    adds more project to portfolio page

--- Portfolio_site.client/app/portfolioPage/portfolioPage.tsx
+++ Portfolio_site.client/app/portfolioPage/portfolioPage.tsx
@@ -16,7 +16,7 @@ export function Portfolio() {
       name: "PokeverseUI",
       url: "https://github.com/eholloway1/pokeverseUI",
       img: null,
-      descr: "This frontend React application is a product from my time in the Multiverse bootcamp. " + 
+      descr: "This frontend React SPA is a product from my time in the Multiverse bootcamp. " + 
         "We were tasked with enabling features ranging from displaying the original 151 pokemon by reading the response object of a get request," +
         "to displaying nested information of each pokemone object returned."
     },
@@ -24,9 +24,32 @@ export function Portfolio() {
       name: "currency_converter",
       url: "https://github.com/eholloway1/currency_converter",
       img: null,
-      descr: "This full-stack app starts up a simple back end with an endpoint to return cart objects on get requests. " +
+      descr: "This full-stack application starts up a simple back end with an endpoint to return cart objects on get requests. " +
         "The frontend retrieves carts, based off the url path, and displays the cart. " +
         "users can then select a currency from a dropdown and see their cart price convert to their selected currency type."
+    },
+    {
+      name: "Noodle Search",
+      url: "https://github.com/Multiverse-frontend-project/NoodleSearch",
+      img: null,
+      descr: "This frontend React SPA allows users to enter in a search query. It then calls a google api with the users search query and appends 'noodle' to it. " +
+        "It then returns the noodlefied images, organized on Material UI cards, of the search query."
+    },
+    {
+      name: "Scooter App",
+      url: "https://github.com/MV-Open-SWE-9/scooter-project-eholloway1",
+      img: null,
+      descr: "This frontend application allows users to create accounts, login and logout, and rent Scooters. " +
+        "Scooters can be stationed, rented, lose charge, recharge, and be labeled as broken. Users are not able to create duplicate accounts, " +
+        "logout if they are not logged in, station Scooters in stations that do not exist or are already stationed."
+    },
+    {
+      name: "Mojo the Summoning (Card game project)",
+      url: "https://github.com/MV-Open-SWE-9/mojo-the-summoning-eholloway1",
+      img: null,
+      descr: "A frontend application that allows each user to create a deck of cards. " +
+        "Each deck may contain many cards, but a card may only belong to one deck. " +
+        "Each card has an attack, and attacks can belong to many cards."
     }
   ];
 
